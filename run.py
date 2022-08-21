593500e92ad5bf96946e17564831f518dc348f15
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-21 00:25:37 -0400

    init

--- run.py
+++ run.py
@@ -8,16 +8,18 @@ st.set_page_config(
 uploaded_file_expected = st.file_uploader("Expected Choose expected file")
 uploaded_file_counted = st.file_uploader("Choose a counted file")
 
-
-
 use_example_file = st.checkbox(
     "Show the chart original data", False, help="be sure to upload the files"
 )
 
+if uploaded_file_expected is not None:
+    df_expected = pd.read_csv(uploaded_file_expected)
+if uploaded_file_counted is not None:    
+    df_counted = pd.read_csv(uploaded_file_counted)
+
 if use_example_file:
 
     if uploaded_file_expected is not None:
-        df_expected = pd.read_csv(uploaded_file_expected)
         st.markdown("### Expected Data preview")
         st.write(df_expected)
     else:
@@ -25,7 +27,6 @@ if use_example_file:
     
 
     if uploaded_file_counted is not None:
-        df_counted = pd.read_csv(uploaded_file_counted)
         st.markdown("### Counted Data preview")
         st.write(df_counted)
     else:    
@@ -34,18 +35,22 @@ if use_example_file:
     
     
 if uploaded_file_expected is not None and  uploaded_file_counted is not None:
-    df_expected = pd.read_csv(uploaded_file_expected)
-    df_counted = pd.read_csv(uploaded_file_counted)
+    removeDuplicate = st.checkbox(
+    "Show data without duplicate", False, help="be sure to upload the files"
+    )
+    # remove duplicates
     df_counted = df_counted.drop_duplicates("RFID")
     df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
     make_choice = st.sidebar.selectbox('Select your RFID:', df_B)
-    st.write('Results:', df_B)
-
+    if removeDuplicate:
+        st.write('Results:', df_B)
+    
+S
 else:
     st.write("there is no data") 
       
 
 
-# remove duplicates
+
 
 
