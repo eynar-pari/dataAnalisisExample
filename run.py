1154d176d598a135a0ade05e235a7eb0f67b3dd6
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-20 23:44:39 -0400

    fix

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ import streamlit as st
 st.title("Mojix Bootcam - Excercise 3")
 
 uploaded_file_expected = st.file_uploader("Expected Choose expected file")
+uploaded_file_counted = st.file_uploader("Choose a counted file")
+
 if uploaded_file_expected is not None:
   df = pd.read_csv(uploaded_file_expected)
 
-
-  uploaded_file_counted = st.file_uploader("Choose a counted file")
 if uploaded_file_counted is not None:
   df = pd.read_csv(uploaded_file_counted)
