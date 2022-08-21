bef7fa351ed3a42b65a886b365bd4c70e0cc96d1
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-20 23:34:22 -0400

    init

--- run.py
+++ run.py
@@ -1,20 +1,12 @@
 import streamlit as st
 import pandas as pd 
-import seaborn as sns
 import matplotlib.pyplot as plt  
 
 
 st.title('CSV Reader')
 file = st.file_uploader('upload a csv',type="csv")
-if file:
-    df = pd.read_csv(file)
-    st.dataframe(df)
-    st.markdown("---")
-    fig1 = plt.figure(figsize=(10,4))
-    sns.countplot(x='Pclass',data=df)
-    st.pyplot(fig1)
+st.write(df)
 
-    fig2 = plt.figure()
 
 #
 #  to execute
