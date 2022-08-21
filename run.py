4885825b1a6de07bdc3068e172e5281a998d34e3
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-21 01:12:28 -0400

    duplicate

--- run.py
+++ run.py
@@ -30,36 +30,33 @@ if start_button :
         df_expected = pd.read_csv(uploaded_file_expected, encoding="latin-1", dtype=str)
     if uploaded_file_counted:
         df_counted = pd.read_csv(uploaded_file_counted, encoding="latin-1", dtype=str)
-    if df_expected and df_counted:
-        # remove duplicates
-        df_counted = df_counted.drop_duplicates("RFID")
-        df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
-        make_choice = st.sidebar.selectbox('Select your RFID:', df_B)
+    # remove duplicates
+    df_counted = df_counted.drop_duplicates("RFID")
+    df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+    make_choice = st.sidebar.selectbox('Select your RFID:', df_B)
 
-        st.write('Result without Duplicates:', df_B)
-        # aggregate
-        my_cols_selected = ["Retail_Product_Color",
-                            "Retail_Product_Level1",
-                            "Retail_Product_Level1Name",
-                            "Retail_Product_Level2Name",
-                            "Retail_Product_Level3Name",
-                            "Retail_Product_Level4Name",
-                            "Retail_Product_Name",
-                            "Retail_Product_SKU",
-                            "Retail_Product_Size",
-                            "Retail_Product_Style",
-                            "Retail_SOHQTY"]
+    st.write('Result without Duplicates:', df_B)
+    # aggregate
+    my_cols_selected = ["Retail_Product_Color",
+                        "Retail_Product_Level1",
+                        "Retail_Product_Level1Name",
+                        "Retail_Product_Level2Name",
+                        "Retail_Product_Level3Name",
+                        "Retail_Product_Level4Name",
+                        "Retail_Product_Name",
+                        "Retail_Product_SKU",
+                        "Retail_Product_Size",
+                        "Retail_Product_Style",
+                        "Retail_SOHQTY"]
 
-        df_A = df_expected[my_cols_selected]    
-        df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
-        df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0)
-        df_discrepancy["Retail_CCQTY"] = df_discrepancy["Retail_CCQTY"].astype(int)
-        df_discrepancy["Retail_SOHQTY"] = df_discrepancy["Retail_SOHQTY"].fillna(0).astype(int)
-        df_discrepancy["Diff"] = df_discrepancy["Retail_CCQTY"] - df_discrepancy["Retail_SOHQTY"]
-        df_discrepancy.loc[df_discrepancy["Diff"]<0, "Unders"] = df_discrepancy["Diff"] * (-1)
-        df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int)
-        df_final = df_discrepancy.groupby("Retail_Product_Level1Name").sum()
-        st.write('Agregate Final:', df_final)
+    df_A = df_expected[my_cols_selected]    
+    df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
+    df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0)
+    df_discrepancy["Retail_CCQTY"] = df_discrepancy["Retail_CCQTY"].astype(int)
+    df_discrepancy["Retail_SOHQTY"] = df_discrepancy["Retail_SOHQTY"].fillna(0).astype(int)
+    df_discrepancy["Diff"] = df_discrepancy["Retail_CCQTY"] - df_discrepancy["Retail_SOHQTY"]
+    df_discrepancy.loc[df_discrepancy["Diff"]<0, "Unders"] = df_discrepancy["Diff"] * (-1)
+    df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int)
+    df_final = df_discrepancy.groupby("Retail_Product_Level1Name").sum()
+    st.write('Agregate Final:', df_final)
 
-    else:
-        st.write('Please uplaod a file or use the default data checkbox')
