2a8fe639b9f8ead1f12b3a95fa6472a79ce0abc2
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-20 23:58:46 -0400

    data

--- run.py
+++ run.py
@@ -1,16 +1,37 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Mojix Bootcam - Excercise 3")
+st.set_page_config(
+    page_title="Bootcam Mojix", page_icon="📊", initial_sidebar_state="expanded"
+)
 
 uploaded_file_expected = st.file_uploader("Expected Choose expected file")
 uploaded_file_counted = st.file_uploader("Choose a counted file")
 
-if uploaded_file_expected is not None:
-  expected = pd.read_csv(uploaded_file_expected)
 
-if uploaded_file_counted is not None:
-  counted = pd.read_csv(uploaded_file_counted)
 
-st.write(expected)
-st.write(counted)
+use_example_file = st.checkbox(
+    "Show the chart original data", False, help="be sure to upload the files"
+)
+
+if use_example_file:
+
+    if uploaded_file_expected is not None:
+        df_expected = pd.read_csv(uploaded_file_expected)
+
+
+    if uploaded_file_counted is not None:
+        df_counted = pd.read_csv(uploaded_file_counted)
+    
+    st.markdown("### Expected Data preview")
+    st.write(expected)
+    st.markdown("### Counted Data preview")
+    st.write(counted)
+      
+
+# remove duplicates
+df_counted = df_counted.drop_duplicates("RFID")
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+
+st.write(df_B)
+
