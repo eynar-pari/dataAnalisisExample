a1d9b74c212fc45332c43ad426fb508a4ed4c7ed
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-21 00:28:02 -0400

    duplicate

--- run.py
+++ run.py
@@ -8,32 +8,11 @@ st.set_page_config(
 uploaded_file_expected = st.file_uploader("Expected Choose expected file")
 uploaded_file_counted = st.file_uploader("Choose a counted file")
 
-use_example_file = st.checkbox(
-    "Show the chart original data", False, help="be sure to upload the files"
-)
-
 if uploaded_file_expected is not None:
     df_expected = pd.read_csv(uploaded_file_expected)
 if uploaded_file_counted is not None:    
     df_counted = pd.read_csv(uploaded_file_counted)
 
-if use_example_file:
-
-    if uploaded_file_expected is not None:
-        st.markdown("### Expected Data preview")
-        st.write(df_expected)
-    else:
-        st.write("please upload the expected file")  
-    
-
-    if uploaded_file_counted is not None:
-        st.markdown("### Counted Data preview")
-        st.write(df_counted)
-    else:    
-        st.write("please upload the counted file") 
-
-    
-    
 if uploaded_file_expected is not None and  uploaded_file_counted is not None:
     removeDuplicate = st.checkbox(
     "Show data without duplicate", False, help="be sure to upload the files"
@@ -44,3 +23,5 @@ if uploaded_file_expected is not None and  uploaded_file_counted is not None:
     make_choice = st.sidebar.selectbox('Select your RFID:', df_B)
     if removeDuplicate:
         st.write('Results:', df_B)
+
+        
