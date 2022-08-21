95f597a0550a2024180d46babdb098154e629a1e
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-20 23:46:14 -0400

    show data

--- run.py
+++ run.py
@@ -7,7 +7,10 @@ uploaded_file_expected = st.file_uploader("Expected Choose expected file")
 uploaded_file_counted = st.file_uploader("Choose a counted file")
 
 if uploaded_file_expected is not None:
-  df = pd.read_csv(uploaded_file_expected)
+  expected = pd.read_csv(uploaded_file_expected)
 
 if uploaded_file_counted is not None:
-  df = pd.read_csv(uploaded_file_counted)
+  counted = pd.read_csv(uploaded_file_counted)
+
+st.write(expected)
+st.write(counted)
